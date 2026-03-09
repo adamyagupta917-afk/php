05008e812d2f2e18257ede395273616f0b2a5a2d
adamyagupta917-afk <adamyagupta917@gmail.com>
2026-03-09 11:41:48 +0530

    Add files via upload

---
+++ sumdigit.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $n = 1456;
+    $sum=0;
+    while($n>0){
+        $rem=$n%10;
+        $sum=$sum+$rem;
+        $n=(int)($n/10);
+    }
+    echo"sum of digit=".$sum;
+    ?>
+</body>
+</html>
